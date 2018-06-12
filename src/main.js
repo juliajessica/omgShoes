49b8e5d61e796a4ddece53ca662308b10ea2978b
Student <example@example.com>
2018-06-12 08:09:28 -0700

    create repository with all files

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// import { } from './scripts.js';
+import './input.scss';
+import $ from 'jquery';
+// import './styles.css';
+
+$(document).ready(function(){
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+     { from: 'src/img', to: 'img'}
+    ]),
+    new UglifyJsPlugin({ sourceMap: true }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Title Words',
+      template: './src/index.html',
+      inject: 'body'
+    }),
+    new Dotenv(),
+    new ExtractTextPlugin({filename:'app.bundle.css'})
+  ],
+  module: {
+    rules: [
+      {
+      test: /\.(s*)css$/,
+        use: ExtractTextPlugin.extract({
+          fallback:'style-loader',
+          use:['css-loader','sass-loader'],
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
+        loader: "eslint-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
+        loader: "babel-loader",
+        options: {
+        presets: ['es2015']
+        }
+      }
+   ]
+ }
+};
